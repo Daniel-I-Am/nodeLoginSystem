6aa29f61f652cec8e91983be3482b2a4edacbfc2
Daniel-I-Am <djdecloet@gmail.com>
2018-09-24 16:38:06 +0200

    proper migration

--- populate.js
+++ populate.js
@@ -2,6 +2,14 @@ const sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./database.sqlite3');
 
 db.serialize(function () {
+    //db.run('CREATE TABLE saves (id INT, content TEXT)');
+    var stmt = db.prepare('INSERT INTO saves (id, content) VALUES (?, ?)');
+
+    for (let i = 0; i < 10; i++) {
+        stmt.run(i.toString(), "'someText" + i.toString() + "'");
+    }
+
+    stmt.finalize();
 })
 
 db.close()
