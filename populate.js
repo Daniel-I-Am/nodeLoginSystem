f984a598b0fd7b522c38d5fd84a62179fd1f7b4f
Daniel-I-Am <djdecloet@gmail.com>
2018-09-24 16:51:54 +0200

    better pop

--- populate.js
+++ populate.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./database.sqlite3');
 
 db.serialize(function () {
-    //db.run('CREATE TABLE saves (id INT, content TEXT)');
+    if (process.argv[2] == "c") { db.run('CREATE TABLE saves (id INT, content TEXT)'); }
     var stmt = db.prepare('INSERT INTO saves (id, content) VALUES (?, ?)');
 
     for (let i = 0; i < 10; i++) {
