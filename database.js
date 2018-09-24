95a5324e68ec78ea157d7fae686c78d5a04d1f22
Daniel-I-Am <djdecloet@gmail.com>
2018-09-24 16:17:59 +0200

    start transfer to sqlite3

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-var NoSQL = require('nosql');
+/*var NoSQL = require('nosql');
 var db = NoSQL.load('./database.nosql');
  
 // db === Database instance <https://docs.totaljs.com/latest/en.html#api~Database>
@@ -7,6 +7,7 @@ function request(url) {
     console.log(url);
 
     var queryparams = url.split('?')[1];
+    if (queryparams == undefined) return;
     var params = queryparams.split('&');
 
     var pair = null,
@@ -20,6 +21,8 @@ function request(url) {
     switch (data.requestType) {
         case "load":
             return loadSaveGame(data.id);
+        case "save":
+            return saveSaveGame(data.id, data.content);
         default:
             return '{"error": "Cannot understand request type."}';
     }
@@ -39,4 +42,20 @@ function loadSaveGame(uid) {
     return '{"error": "Errored on database extraction"}';
 }
 
+function saveSaveGame(uid, data) {
+    db.insert()
+    db.find().make(function(filter) {
+        filter.where('id', uid);
+        filter.callback(function(err, response) {
+            if (err == null) {
+                return response;
+            } else {
+                return '{"error": "' + err + '"}';
+            }
+        });
+    });
+    return '{"error": "Errored on database insertion"}';
+}
+
 module.exports = {"request": request};
+*/
