d893064ab8d4b34242f7c4fcc0e8a297db5b43a7
Daniel de Cloet <djdecloet@gmail.com>
2018-09-24 12:42:23 +0200

    testing server

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-var NoSQL = require('nosql');
-var nosql = NoSQL.load('./database.nosql');
+// var NoSQL = require('nosql');
+// var nosql = NoSQL.load('./database.nosql');
  
-// db === Database instance <https://docs.totaljs.com/latest/en.html#api~Database>
+// // db === Database instance <https://docs.totaljs.com/latest/en.html#api~Database>
  
-db.find().make(function(filter) {
-    filter.where('age', '>', 20);
-    filter.where('removed', false);
-    filter.callback(function(err, response) {
-        console.log(err, response);
-    });
+// nosql.find().make(function(filter) {
+//     filter.where('age', '>', 20);
+//     filter.where('removed', false);
+//     filter.callback(function(err, response) {
+//         console.log(err, response);
+//     });
+// });
+
+//Load HTTP module
+const http = require("http");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
