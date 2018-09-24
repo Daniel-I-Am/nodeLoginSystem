5e61407d18cdeb13cc18f0d244dda107b21bc0cc
Daniel de Cloet <djdecloet@gmail.com>
2018-09-24 12:11:27 +0200

    add content

---
+++ app.js
@@ -0,0 +1,12 @@
+var NoSQL = require('nosql');
+var db = NoSQL.load('/path/to/database.nosql');
+ 
+// db === Database instance <https://docs.totaljs.com/latest/en.html#api~Database>
+ 
+db.find().make(function(filter) {
+    filter.where('age', '>', 20);
+    filter.where('removed', false);
+    filter.callback(function(err, response) {
+        console.log(err, response);
+    });
+});
