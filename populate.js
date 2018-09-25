71a226adf4e502d1be6c73015b34999cca1b0a94
Daniel-I-Am <djdecloet@gmail.com>
2018-09-25 21:33:34 +0200

    adjust populate for login structure

--- populate.js
+++ populate.js
@@ -13,7 +13,7 @@ db.serialize(function () {
     // if we parse a `c` value, create new table
     if (process.argv[2].match(".*c.*")) {
         db.run('CREATE TABLE saves (id INT UNIQUE, user TEXT, content TEXT)');
-        db.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)');
+        db.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, salt TEXT)');
     }
 
     // prepare insert statement
@@ -23,17 +23,6 @@ db.serialize(function () {
     let users = ["dan", "kevin", "someother user", "right!!'!!", "');drop table saves;--", "u5", "u6", "u7", "u8", "u9"];
     for (let i = 0; i < 10; i++) {
         stmt.run(i.toString(), users[i], "someText" + i.toString());
-    }
-
-    // finalize query
-    stmt.finalize();
-    
-    // prepare insert statement
-    var stmt = db.prepare('INSERT OR REPLACE INTO users (username, password) VALUES (?, ?)');
-
-    // fill database with random data
-    for (let i = 0; i < 10; i++) {
-        stmt.run(users[i], "pass");
     }
 
     // finalize query
