cb4cee22e682e7edbe4e756ea56caca752540c25
Daniel-I-Am <djdecloet@gmail.com>
2018-09-25 19:41:17 +0200

    implementation users.js

---
+++ users.js
@@ -0,0 +1,11 @@
+function register(request, callback) {
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+    request.on('end', () => {
+        callback('text/plain', body);
+    });
+}
+
+module.exports = {"register": register}
