e70fd2a542357e0b4be254bb67d2b28b89008271
Daniel-I-Am <djdecloet@gmail.com>
2018-09-25 20:32:26 +0200

    adding parsing and DB interaction

--- users.js
+++ users.js
@@ -6,7 +6,44 @@ function register(request, callback) {
         body += chunk.toString(); // convert Buffer to string
     });
     request.on('end', () => {
-        callback('text/plain', body);
+        let queryparams = body;
+        var params = queryparams.split('&');
+    
+        // put all GET params into an array
+        var pair = null,
+            data = [];
+    
+        params.forEach(function(d) {
+            pair = d.split('=');
+            data[pair[0]] = pair[1];
+        });
+        saveUser(data)
+    });
+}
+
+function saveUser(data) {
+    let username = data.username,
+        password = data.password;
+    if (!username || !password) {
+        callback('application/json', '{"error": "Username or password not provided"}')
+        return;
+    }
+    // once again, set a timeout callback, we cancel that if everything's handled properly
+    var timeout = setTimeout(function() {
+        callback('application/json', '{"error": "timeout"}')
+    }, 1000)
+    // start DB interaction
+    db.serialize(function() {
+        // use some SQL magic to insert a new value into the table, but if the ID already exist, replace the existing save slot
+        let stmt = db.prepare("INSERT OR REPLACE INTO users (name, password) VALUES (?, ?)");
+        // execute prepared statement with our data
+        stmt.run(username, password);
+        // finish up query
+        stmt.finalize();
+        // cancel callback
+        clearTimeout(timeout);
+        // report back that there's no error in JSON
+        callback('application/json', '{"error": null}');
     });
 }
 
