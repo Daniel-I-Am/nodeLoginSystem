ac91e7f2231bbd82a260d7d097c847d0556d75b0
Daniel-I-Am <djdecloet@gmail.com>
2018-09-25 20:41:39 +0200

    new DB structure users table

--- populate.js
+++ populate.js
@@ -6,9 +6,15 @@ var db = new sqlite3.Database('./database.sqlite3');
 // start DB interactionn
 db.serialize(function () {
     // if we parse a `d` value, drop current table
-    if (process.argv[2].match(".*d.*")) { db.run('DROP TABLE saves'); }
+    if (process.argv[2].match(".*d.*")) {
+        db.run('DROP TABLE saves');
+        db.run('DROP TABLE users');
+    }
     // if we parse a `c` value, create new table
-    if (process.argv[2].match(".*c.*")) { db.run('CREATE TABLE saves (id INT UNIQUE, user TEXT, content TEXT)'); }
+    if (process.argv[2].match(".*c.*")) {
+        db.run('CREATE TABLE saves (id INT UNIQUE, user TEXT, content TEXT)');
+        db.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)');
+    }
 
     // prepare insert statement
     var stmt = db.prepare('INSERT OR REPLACE INTO saves (id, user, content) VALUES (?, ?, ?)');
@@ -17,6 +23,17 @@ db.serialize(function () {
     let users = ["dan", "kevin", "someother user", "right!!'!!", "');drop table saves;--", "u5", "u6", "u7", "u8", "u9"];
     for (let i = 0; i < 10; i++) {
         stmt.run(i.toString(), users[i], "someText" + i.toString());
+    }
+
+    // finalize query
+    stmt.finalize();
+    
+    // prepare insert statement
+    var stmt = db.prepare('INSERT OR REPLACE INTO users (username, password) VALUES (?, ?)');
+
+    // fill database with random data
+    for (let i = 0; i < 10; i++) {
+        stmt.run(users[i], "pass");
     }
 
     // finalize query
