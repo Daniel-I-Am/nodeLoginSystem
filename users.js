4b2578b4f273bd3b4771ab15d5738ded3338cc85
Daniel-I-Am <djdecloet@gmail.com>
2018-09-26 19:42:09 +0200

    guid function

--- users.js
+++ users.js
@@ -1,6 +1,15 @@
 const db = require("./database.js").database;
 const sha256 = require("js-sha256");
 
+function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 function register(request, callback) {
     let body = '';
     request.on('data', chunk => {
