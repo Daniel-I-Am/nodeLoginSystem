f18667643e859ca152a8bbac4c2b6bfe0e83bd59
Daniel-I-Am <djdecloet@gmail.com>
2018-10-04 19:16:58 +0200

    database interaction register

--- register.js
+++ register.js
@@ -5,7 +5,7 @@ const login = require("./login.js");
 // import node modules
 const sha256 = require("js-sha256");
 
-function register(request, callback) {
+async function register(request, callback) {
     let body = '';
     request.on('data', function (data) {
         body += data;
@@ -14,15 +14,25 @@ function register(request, callback) {
         if (body.length > 1e6)
             request.connection.destroy();
     });
-    request.on('end', function () {
+    request.on('end', async function () {
         try {
             var post = JSON.parse(body)
             // use post['blah'], etc.
-            callback('application/json', JSON.stringify({"username": post['username'], "password": post['password']}));
+            await registerDB(post.username, post.password, callback)
         } catch(err) {
-            callback('applcation/json', JSON.stringify({"error": err.message}))
+            callback('application/json', JSON.stringify({"error": err.message}))
         }
     });
+}
+
+async function registerDB(username, password, callback) {
+    let salt = "asd"
+    try {
+        db.insert("users", {"username": username, "password": password, "salt": salt}, 
+            function(data,err) {callback('application/json', JSON.stringify(err ? {"error": err.message} : {"data": data}))}, false);
+    } catch(err) {
+        callback('application/json', JSON.stringify({"error": err.message}));
+    }
 }
 
 module.exports = {"register": register}
