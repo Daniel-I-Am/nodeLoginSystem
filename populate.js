244c4f20857b5e0d3b6a906f136f5fa81fc2787e
Daniel-I-Am <djdecloet@gmail.com>
2018-10-04 21:36:33 +0200

    remake DB structure

--- populate.js
+++ populate.js
@@ -13,9 +13,9 @@ db.serialize(function () {
     }
     // if we parse a `c` value, create new table
     if (process.argv[2].match(".*c.*")) {
-        db.run('CREATE TABLE games (gameID INT AUTO_INCREMENT UNIQUE PRIMARY KEY, userID INT, data TEXT)');
-        db.run('CREATE TABLE tokens (userID INT UNIQUE PRIMARY KEY, ip TEXT, token TEXT)');
-        db.run('CREATE TABLE users (userID INTEGER AUTO_INCREMENT UNIQUE PRIMARY KEY, username TEXT UNIQUE, password TEXT, salt TEXT)');
+        db.run('CREATE TABLE games (userID INTEGER NOT NULL, data TEXT)');
+        db.run('CREATE TABLE tokens (userID INTEGER UNIQUE PRIMARY KEY NOT NULL, token TEXT NOT NULL)');
+        db.run('CREATE TABLE users (username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, salt TEXT NOT NULL)');
     }
 })
 
