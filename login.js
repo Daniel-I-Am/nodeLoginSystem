b1fb18aaf6dd2a42a202e56b2753910ee0293697
Daniel-I-Am <djdecloet@gmail.com>
2018-10-04 21:45:24 +0200

    comments

--- login.js
+++ login.js
@@ -27,35 +27,53 @@ async function login(request, callback) {
         try {
             var post = JSON.parse(body)
             // use post['blah'], etc.
+
+            // get the plain text password sent over the wire
             let plainTextPassword = post.password;
+            // select the salt that should be stored in the DB
             db.select("users", ["salt"], {"username": post.username}, async function(data, err) {
+                // Incorrect username
                 if (data.length == 0) {
                     callback('application/json', JSON.stringify({"error": "Username or password incorrect"}))
                 }
+                // get the salt that was selected
                 salt = data[0].salt
+                // hash and salt the password with the selected salt
                 let password = sha256.sha256(salt+plainTextPassword);
+                // select every detail about the user if the username AND hashed/salted password match
                 db.select("users", ["*"], {"username": post.username, "password": password}, async function(data, err) {
+                    // incorrect password
                     if (data.length == 0) {
                         callback('application/json', JSON.stringify({"error": "Username or password incorrect"}))
+                    // login details are correct
                     } else {
+                        // if there's an error in the query, return it
                         if (err) {
                             callback('application/json', JSON.stringify({"error": err.message}))
+                        // no error, all was fine
                         } else {
+                            // delete the password and salt from the data[0] object, the client side does not need to know those
                             delete data[0].password;
                             delete data[0].salt;
+                            // generate a login token and append it onto the data[0] object
                             data[0].token = guid();
+                            // insert the new token and userID into the tokens table
                             db.insert("tokens", {"userID": data[0].rowid, "token": data[0].token}, function(_, err) {
                                 if (err) {
+                                    // return if there were any errors
                                     callback('application/json', JSON.stringify({"error": err.message}))
                                 } else {
+                                    // return the data, login successful
                                     callback('application/json', JSON.stringify(data[0]))
                                 }
-                            }, true);
+                            // true === canOverwrite (in DB.insert)
+                            }, true); // \db.insert
                         }
                     }
-                });
-            });
+                }); // \db.select
+            }); // \db.select
         } catch(err) {
+            // if the original salt selection or anything else going on in the function failed somehow, return the error
             callback('application/json', JSON.stringify({"error": err.message}))
         }
     });
--- register.js
+++ register.js
@@ -19,20 +19,27 @@ async function register(request, callback) {
             var post = JSON.parse(body)
             // use post['blah'], etc.
             let plainTextPassword = post.password,
+                // generate a random salt
                 salt = Math.random().toString(36).substring(7);
+            // salt and hash the password
             let password = sha256.sha256(salt+plainTextPassword);
+            // call next func
             await registerDB(post.username, password, salt, callback)
         } catch(err) {
+            // return error, if one occurs
             callback('application/json', JSON.stringify({"error": err.message}))
         }
     });
 }
 
 async function registerDB(username, password, salt, callback) {
+    // attempt, but catch any errors
     try {
+        // try to insert the new user details into the database
         db.insert("users", {"username": username, "password": password, "salt": salt}, 
             function(data,err) {callback('application/json', JSON.stringify(err ? {"error": err.message} : {"data": data}))}, false);
     } catch(err) {
+        // catch any errors, including `SQLITE_RESTRAINT: UNIQUE`
         callback('application/json', JSON.stringify({"error": err.message}));
     }
 }
