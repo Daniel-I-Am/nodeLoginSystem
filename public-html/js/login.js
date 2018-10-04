232c1b1ece2c49e588864a7f72267510b09a0d36
Daniel-I-Am <djdecloet@gmail.com>
2018-10-04 23:04:50 +0200

    login token and cookies

--- public-html/js/login.js
+++ public-html/js/login.js
@@ -3,7 +3,16 @@ $('#loginForm').submit(async e => {
     e.preventDefault();
     await fetchFrom('/login', 
         {"username": e.target.username.value, "password": e.target.password.value}, 
-        function(data) {console.log(data);}, 
+        function(data) {
+            if (data.error) {
+                console.error("Username or Password Incorrect");
+                return;
+            }
+            console.log(data);
+            setCookie("username", data.username, null);
+            setCookie("token", data.token, null);
+            window.location.replace("/");
+        }, 
         function(err) {console.error(err);}
     );
 });
--- public-html/js/register.js
+++ public-html/js/register.js
@@ -3,7 +3,19 @@ $('#registerForm').submit(async e => {
     e.preventDefault();
     await fetchFrom('/register', 
         {"username": e.target.username.value, "password": e.target.password.value}, 
-        function(data) {console.log(data);}, 
+        async function(data) {
+            console.log(data);
+            await fetchFrom('/login', 
+                {"username": e.target.username.value, "password": e.target.password.value}, 
+                function(data) {
+                    console.log(data);
+                    setCookie("username", data.username, null);
+                    setCookie("token", data.token, null);
+                    window.location.replace("/");
+                }, 
+                function(err) {console.error(err);}
+            );
+        }, 
         function(err) {console.error(err);}
     );
 });
