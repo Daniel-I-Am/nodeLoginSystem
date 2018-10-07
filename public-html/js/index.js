0acb55d5320aee1fe28c62280f8e5d3a9cdd74a8
Daniel-I-Am <djdecloet@gmail.com>
2018-10-07 15:36:14 +0200

    applied navbar everywhere

--- public-html/js/index.js
+++ public-html/js/index.js
@@ -1,18 +1,3 @@
 $("#cookies").each(function( index ) {
     $(this)[0].innerHTML = `${getCookie("username")}<br />${getCookie("token")}`;
 });
-
-$(".logout").click(function(e) {
-    e.preventDefault();
-    fetchFrom('/logout', 
-        {"username": getCookie("username"), "token": getCookie("token")}, 
-        function(data) {
-            eraseCookie("username");
-            eraseCookie("token");
-            window.location.reload(true);
-        }, 
-        function(err) {
-            console.error(err);
-        }
-    );
-})
