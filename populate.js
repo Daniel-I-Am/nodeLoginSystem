0b9cbd6fd6e0c4e46edb5fb47681e0137f6b0472
Daniel-I-Am <djdecloet@gmail.com>
2018-10-08 17:58:11 +0200

    usage of CMDLine Parser into populate.js

--- populate.js
+++ populate.js
@@ -1,3 +1,4 @@
+const CMDLineArgs = parseCMDLine();
 // import sqlite
 const sqlite3 = require('sqlite3').verbose();
 // open DB
@@ -6,13 +7,13 @@ var db = new sqlite3.Database('./database.sqlite3');
 // start DB interactionn
 db.serialize(function () {
     // if we parse a `d` value, drop current table
-    if (process.argv[2].match(".*d.*")) {
+    if (CMDLineArgs.flags.includes('d')) {
         db.run('DROP TABLE games');
         db.run('DROP TABLE users');
         db.run('DROP TABLE tokens');
     }
     // if we parse a `c` value, create new table
-    if (process.argv[2].match(".*c.*")) {
+    if (CMDLineArgs.flags.includes('c')) {
         db.run('CREATE TABLE games (userID INTEGER NOT NULL, data TEXT)');
         db.run('CREATE TABLE tokens (userID INTEGER UNIQUE PRIMARY KEY NOT NULL, token TEXT NOT NULL)');
         db.run('CREATE TABLE users (username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, salt TEXT NOT NULL)');
@@ -21,3 +22,22 @@ db.serialize(function () {
 
 // close DB connection
 db.close();
+
+function parseCMDLine() {
+    flags = []
+    args = []
+    options = {}
+    process.argv.forEach((e, i) => {
+        if (i < 2) return;
+        if (e.startsWith("-") && !e.startsWith("--")) {
+            flags = flags.concat(e.substring(1).split(""));
+            return;
+        }
+        if (e.startsWith("--") && e.includes("=")) {
+            options[e.substring(2).split("=")[0]] = e.substring(2).split("=")[1];
+            return;
+        }
+        args = args.concat(e);
+    });
+    return {"flags": flags, "args": args, "options": options}
+}
